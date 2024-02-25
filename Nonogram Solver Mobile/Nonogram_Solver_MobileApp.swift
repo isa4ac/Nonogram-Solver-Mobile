4675882b7fbd419bd43afe5c64d2e8640e180c60
Isaac Van Meter <ivanmeter28@gmail.com>
2024-02-24 19:04:22 -0500

    Initial Commit

---
+++ Nonogram Solver Mobile/Nonogram_Solver_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nonogram_Solver_MobileApp.swift
+//  Nonogram Solver Mobile
+//
+//  Created by Isaac Vanmeter on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Nonogram_Solver_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
